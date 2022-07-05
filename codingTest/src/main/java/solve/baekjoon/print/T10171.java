4f588d0678ca8178e354d8ddefc90a7bb87ab70a
SungACho <sunga0720@naver.com>
2022-07-05 17:59:33 +0900

    print Stirng practice

---
+++ codingTest/src/main/java/solve/baekjoon/print/T10171.java
@@ -0,0 +1,10 @@
+package solve.baekjoon.print;
+
+public class T10171 {
+	public static void main(String[] args) {
+		System.out.println("\\    /\\ ");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  ) ");
+		System.out.println(" \\(__)| ");
+	}
+}
---
+++ codingTest/src/main/java/solve/baekjoon/print/T2557.java
@@ -0,0 +1,8 @@
+package solve.baekjoon.print;
+
+public class T2557 {
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!");
+	}
+}
