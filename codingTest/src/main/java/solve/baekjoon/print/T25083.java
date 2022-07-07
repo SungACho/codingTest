f8a1c0a5a2baf1672300ce80066e73084f2b8520
SungACho <sunga0720@naver.com>
2022-07-07 12:01:19 +0900

    print practice

---
+++ codingTest/src/main/java/solve/baekjoon/print/T25083.java
@@ -0,0 +1,14 @@
+package solve.baekjoon.print;
+
+public class T25083 {
+	public static void main(String[] args) {
+		String str = "         ,r'\"7\n"
+				   + "r`-_   ,'  ,/\n"
+				   + " \\. \". L_r'\n"
+				   + "   `~\\/\n"
+				   + "      |\n"
+				   + "      |";
+
+		System.out.print(str);
+	}
+}
