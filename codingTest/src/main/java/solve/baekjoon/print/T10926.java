c16e6ff964ef66472f1b9cde959b8647f2e3b948
SungACho <sunga0720@naver.com>
2022-07-07 11:07:26 +0900

    print practice

---
+++ codingTest/src/main/java/solve/baekjoon/print/T10926.java
@@ -0,0 +1,11 @@
+package solve.baekjoon.print;
+import java.util.Scanner;
+
+public class T10926 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String str = sc.nextLine();
+		System.out.println(str+"??!");
+	}
+}
