70db99c6aa0956ebe067dfc6a3e7b0ddb6477236
SungACho <sunga0720@naver.com>
2022-07-11 17:53:09 +0900

    Math practice

---
+++ codingTest/src/main/java/solve/baekjoon/math1/T2869.java
@@ -0,0 +1,30 @@
+package solve.baekjoon.math1;
+import java.util.*;
+import java.io.*;
+
+public class T2869 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		long A = Long.parseLong(st.nextToken());
+		long B = Long.parseLong(st.nextToken());
+		long V = Long.parseLong(st.nextToken());
+
+		long destination = V - B;
+		long dist = A - B;
+
+		long div = destination / dist;
+		long mod = destination % dist;
+
+		long day = div;
+		if(mod != 0) {
+			day = day + 1;
+			System.out.println(day);
+		}
+		else {
+			System.out.println(day);
+		}
+
+	}
+}
