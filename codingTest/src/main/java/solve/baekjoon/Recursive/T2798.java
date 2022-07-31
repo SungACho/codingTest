dc3c54097785973575ca799ffc0e1989c115399f
sungaCho <sunga0720@naver.com>
2022-07-31 15:49:27 +0900

    change location

---
+++ codingTest/src/main/java/solve/baekjoon/Recursive/T2798.java
@@ -0,0 +1,14 @@
+package solve.baekjoon.Recursive;
+
+import java.io.*;
+
+public class T2798 {
+
+	public static void main(String[] args) {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] 
+		
+	}
+}
